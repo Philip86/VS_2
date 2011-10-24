28fa2ecf0da2663649ce2dc7dd9accb082bd78f1
Philip86 <ryo@vanpixel.de>
2011-10-24 17:10:05 +0200

    ggt weitergemacht

--- ggt.erl
+++ ggt.erl
@@ -1,33 +1,42 @@
 -module(ggt).
 -compile(export_all).
 
-start(Mi) -> 
+start(Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit) -> 
+	%KoordName ! hello,
+	%Namensdienst ! {rebind self()}
+
 	receive	
-		{setneighbors, LeftN, RightN} -> 
-			loop(Mi, LeftN, RightN)
+		{setneighbors, LeftN, RightN} -> true
+			%loop(Mi, LeftN, RightN, Terminate)
+	end,
+	receive
+	{setpm, MiNeu} ->
+		loop(MiNeu, LeftN, RightN, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit)
 	end.
 
-loop(Mi, NeighborL, NeighborR) ->
+loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit) ->
 	receive
 		%{setneighbors, LeftN, RightN} -> 
 		%	loop(Mi, LeftN, RightN);
 		{setpm, MiNeu} ->
-			loop(MiNeu, NeighborL, NeighborR);
+			loop(MiNeu, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit);
 		{sendy, Y} ->
-			NewMi = calcGgt(Mi, Y, NeighborL, NeighborR),
-			loop(NewMi, NeighborL, NeighborR);
+			NewMi = calcGgt(Mi, Y, NeighborL, NeighborR, KoordName, MeinName),
+			loop(NewMi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit);
 		{abstimmung,Initiator} -> 
 			%KOMMT NOCH!!!!
-			loop(Mi, NeighborL, NeighborR);
+			loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit);
 		{tellmi,From} -> 
 			From ! Mi,
-			loop(Mi, NeighborL, NeighborR);
+			loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit, TermZeit);
 		kill -> death
 	end.
 
-calcGgt(Mi, Y,LeftN, RightN) ->
+calcGgt(Mi, Y,LeftN, RightN, KoordName, MeinName) ->
 	if Y < Mi -> 
 		NewMi = ((Mi - 1) rem Y) + 1,
+		%MUSS NOCH !!!
+		%KoordName ! {briefmi,{MeinName,NewMi,CZeit}},
 		LeftN ! {setpm, NewMi},
 		RightN ! {setpm, NewMi},
 		NewMi;
