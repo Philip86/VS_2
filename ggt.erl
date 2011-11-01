3c453617cac493ac515da2b908feac43cf36067b
Philip <philip@philip-desktop.(none)>
2011-11-01 22:09:39 +0100

    funzt

--- ggt.erl
+++ ggt.erl
@@ -15,37 +15,55 @@ start(NamensdienstNode, KoordName, MeinName, ArbeitsZeit, TermZeit) ->
 	Nameservice2  ! {self(),{lookup,KoordName}},
 	receive 
 		{Name,Node} -> 
-			{Name, Node} ! {hello, MeinName};
+			Koordi = {Name, Node},
+			Koordi ! {hello, MeinName};
 		not_found -> 
+			Koordi = traurig,
 			io:format("GGT konnte Koordinator nicht finden und beendet sich ganz traurig :(\n")
 	end,
-	io:format("asdlhsadh\n"),
 	receive	
-		{setneighbors, LeftN, RightN} -> true
-			%loop(Mi, LeftN, RightN, Terminate)
+		{setneighbors, LeftN, RightN} -> 
+			Nameservice2  ! {self(),{lookup,LeftN}},
+			receive 
+				{Name1,Node1} -> 
+					LeftName2 = {Name1, Node1};
+				not_found -> 
+					LeftName2 = fehler,
+					io:format("GGT ~p konnte linken Nachbarn nicht finden und beendet sich ganz traurig :(\n", [MeinName])
+			end,
+			Nameservice2  ! {self(),{lookup,RightN}},
+			receive 
+				{Name2,Node2} -> 
+					RightName2 = {Name2, Node2};
+				not_found -> 
+					RightName2 = fehler,
+					io:format("GGT ~p konnte rechten Nachbarn nicht finden und beendet sich ganz traurig :(\n", [MeinName])
+			end
 	end,
-	receive
-		{setpm, MiNeu} ->
-			loop(MiNeu, LeftN, RightN, NamensdienstNode, KoordName, MeinName, ArbeitsZeit * 1000, infinity, TermZeit * 1000)
-	end.
+	
+	loop(-1, LeftName2, RightName2, Nameservice2, Koordi, MeinName, ArbeitsZeit * 1000, infinity, TermZeit * 1000).
+
+
 
 loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,TermZeitDauer, TermZeit) ->
-		Timestamp1 = now(),
+	Timestamp1 = now(),
 	receive
-		%{setneighbors, LeftN, RightN} -> 
-		%	loop(Mi, LeftN, RightN);
 		{setpm, MiNeu} ->
+				io:format("Prozess ~p hat neues Mi erhalten! \n",[MeinName]),
 			loop(MiNeu, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,TermZeit, TermZeit);
+
 		{sendy, Y} ->
 			NewMi = calcGgt(Mi, Y, NeighborL, NeighborR, KoordName, MeinName, ArbeitsZeit),
 			io:format("Neuer MI von: ~p  ist = ~p \n", [MeinName, NewMi]),
 			loop(NewMi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,TermZeit, TermZeit);
+
 		{abstimmung,Initiator} -> 
 			Timestamp2 = now(),
 			if Initiator == self() ->
 				io:format("~p bekommt Anfrageabstimmung von ~p, also von sich selbst \n", [MeinName, Initiator]),
 				NewTermZeit = infinity,
 				CZeit = werkzeug:timeMilliSecond(),
+				io:format("Der schwule Koordname = ~p\n", [KoordName]),
 				KoordName ! {briefterm, {MeinName, Mi, CZeit}};
 				true ->
 					TimeDiff = timer:now_diff(Timestamp2, Timestamp1) / 1000,
@@ -63,12 +81,15 @@ loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,Te
 			end,
 			io:format("\n\n NewtermZeit = ~p \n\n", [NewTermZeit]),
 			loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,NewTermZeit, TermZeit);	
+
 		{tellmi,From} -> 
 			Timestamp2 = now(),
 			From ! Mi,
 			loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,TermZeitDauer, TermZeit);
-		kill -> death
+		kill -> 
+			Namensdienst ! {self(),{unbind,MeinName}}
 	after TermZeitDauer -> 
+			io:format("Zeit abgelaufen!!!!!!!!!!!!!!!! Nachtbar R = ~p\n", [NeighborR]),
 			NeighborR ! {abstimmung, self()},
 			io:format("Prozess ~p startet Terminierungsabstimmung \n", [MeinName]),
 			loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,infinity, TermZeit)
@@ -77,16 +98,19 @@ loop(Mi, NeighborL, NeighborR, Namensdienst, KoordName, MeinName, ArbeitsZeit,Te
 
 calcGgt(Mi, Y,LeftN, RightN, KoordName, MeinName, ArbeitsZeit) ->
 	io:format("CalcGGT Fun von: ~p  mit Mi = ~p   und Y = ~p \n", [MeinName,Mi,Y]),
-	if Y < Mi -> 
-		NewMi = ((Mi - 1) rem Y) + 1,
-		timer:sleep(ArbeitsZeit),
-		CZeit = werkzeug:timeMilliSecond(),
-		KoordName ! {briefmi,{MeinName, NewMi, CZeit}},
-		LeftN ! {sendy, NewMi},
-		RightN ! {sendy, NewMi},
-		NewMi;
-		true -> Mi
-	end. 
+	if 
+		Y < Mi -> 
+			NewMi = ((Mi - 1) rem Y) + 1,
+			timer:sleep(ArbeitsZeit),
+			CZeit = werkzeug:timeMilliSecond(),
+			KoordName ! {briefmi,{MeinName, NewMi, CZeit}},
+			LeftN ! {sendy, NewMi},
+			RightN ! {sendy, NewMi},
+			NewMi;
+		true ->
+			Mi
+	end.
+ 
 %{Eine Nachricht <y> ist eingetroffen}
  % if y < Mi 
   %  then Mi := mod(Mi-1,y)+1;
--- koordinator.erl
+++ koordinator.erl
@@ -17,31 +17,47 @@ start() ->
 	io:format("Nameservice: ~p, NameserviceNode: ~p~n", [KAB, NameServiceNode]),
 	timer:sleep(500),
 	KAB ! {self(),{rebind,KoordinatorName,node()}},
-	register(KoordinatorName, spawn(koordinator, init, [ArbeitsZeit, TermZeit, GGTProzessNummer, []])).
+	register(KoordinatorName, spawn(koordinator, init, [ArbeitsZeit, TermZeit, GGTProzessNummer, [], KAB])).
 
-init(ArbeitsZeit, TermZeit, GGTProzessnummer, GGTListe) ->
+init(ArbeitsZeit, TermZeit, GGTProzessnummer, GGTListe, Nameservice) ->
 	receive
 		{getsteeringval, Starter} -> %Die Anfrage nach den steuernden Werten durch den Starter Prozess.
 			Starter ! {steeringval, ArbeitsZeit, TermZeit, GGTProzessnummer},
-			init(ArbeitsZeit, TermZeit, GGTProzessnummer, GGTListe);
+			init(ArbeitsZeit, TermZeit, GGTProzessnummer, GGTListe, Nameservice);
 		
 		{hello, Clientname} -> %Ein ggT-Prozess meldet sich beim Koordinator mit Namen Clientname an (Name ist der lokal registrierte Name!).		
-			init(ArbeitsZeit, TermZeit, GGTProzessnummer, [Clientname | GGTListe]);
+			init(ArbeitsZeit, TermZeit, GGTProzessnummer, [Clientname | GGTListe], Nameservice);
 		letsgo ->
-			createRingAndMis(GGTListe), 
-			startCalc(GGTListe),
-			loop()	
+			createRing(GGTListe, Nameservice), 
+			loop(GGTListe, Nameservice)	
 	end.
 
-createRingAndMis(GGTListe)->
+createMis([], _) ->
+	done;
+createMis([Kopf| Rest], Nameservice)->
+	io:format("Kopf: ~p \n",[Kopf]),
+	Mi = calcRandomMi(),
+	Nameservice  ! {self(),{lookup,Kopf}},
+	receive 
+		{Name,Node} -> 
+			 GGTProzess = {Name, Node};
+		not_found -> 
+			GGTProzess = failed,
+			io:format("GGT konnte Koordinator nicht finden und beendet sich ganz traurig :(\n")
+	end,
+	GGTProzess ! {setpm, Mi},
+	createMis(Rest, Nameservice).
+	
+
+createRing(GGTListe, Nameservice)->
 	Arr = array:from_list(GGTListe),
 	ArrSize = array:size(Arr),
-	for_loop(0, ArrSize, Arr).
+	for_loop(0, ArrSize, Arr, Nameservice).
 	
-for_loop(N, Length, _) when N >= Length -> 
+for_loop(N, Length, _, _) when N >= Length -> 
     io:format("Done!~n");
 
-for_loop(N, Length, Arr) -> 
+for_loop(N, Length, Arr, Nameservice) -> 
 	Current = array:get(N, Arr),
 	if N == 0 ->
 		LeftN = array:get(Length -1, Arr),
@@ -54,10 +70,16 @@ for_loop(N, Length, Arr) ->
 		RightN = array:get(N +1, Arr)
 	end,
 	io:format("Links: ~p ERHIER!!! ~p  Rechts: ~p \n",[LeftN, Current, RightN]),
-	Current ! {setneighbors, LeftN, RightN},
-	Mi = calcRandomMi(),
-	Current ! {setpm, Mi},
-    for_loop(N+1, Length, Arr).
+	Nameservice  ! {self(),{lookup, Current}},
+	receive 
+		{Name,Node} -> 
+			 GGTProzess = {Name, Node};
+		not_found -> 
+			GGTProzess = failed,
+			io:format("GGT konnte Koordinator nicht finden und beendet sich ganz traurig :(\n")
+	end,
+	GGTProzess ! {setneighbors, LeftN, RightN},
+    for_loop(N+1, Length, Arr, Nameservice).
 
 calcRandomMi()->
 	Prims = [3, 5, 11, 13, 23, 37],
@@ -69,42 +91,57 @@ calcRandomMiRec([Kopf | Rest], Produkt) ->
 	ProduktNew = Produkt * math:pow(Kopf, random:uniform(3)-1),
 	calcRandomMiRec(Rest, ProduktNew).
 
-startCalc(GGTListe)->
+startCalc(GGTListe, Nameservice)->
 	Arr = array:from_list(GGTListe),
 	ArrSize = array:size(Arr),
 	AnzStarter = trunc((ArrSize/100) * 15),
 	if AnzStarter < 2 ->
-		sendStartToClients(Arr, 2, [], ArrSize);
-	true -> sendStartToClients(Arr, AnzStarter, [], ArrSize)
+		sendStartToClients(Arr, 2, [], ArrSize, Nameservice);
+	true -> sendStartToClients(Arr, AnzStarter, [], ArrSize, Nameservice)
 	end.
 
-sendStartToClients(Arr, 0, UsedClients, ArrSize) -> 
+sendStartToClients(Arr, 0, UsedClients, ArrSize, Nameservice) -> 
 	io:format("Koordinator hat das zufaellige Starten abgeschlossen \n");
-sendStartToClients(Arr, N, UsedClients, ArrSize) ->
+sendStartToClients(Arr, N, UsedClients, ArrSize, Nameservice) ->
 	ChoosenClient = random:uniform(ArrSize) -1,
 	WasteBool = lists:member(ChoosenClient, UsedClients),
 	if WasteBool ->
-		sendStartToClients(Arr, N, UsedClients, ArrSize);
+		sendStartToClients(Arr, N, UsedClients, ArrSize, Nameservice);
 	true ->
 		Client = array:get(ChoosenClient, Arr),
 		RandomY =  calcRandomMi(),
 		io:format("Koordinator schickt start an: ~p \n", [Client]),
-		Client ! {sendy, RandomY},
-		sendStartToClients(Arr, N-1, [ChoosenClient| UsedClients], ArrSize)
+		Nameservice  ! {self(),{lookup,Client}},
+		receive 
+			{Name,Node} -> 
+				 GGTProzess = {Name, Node};
+			not_found -> 
+				GGTProzess = failed,
+				io:format("GGT konnte Koordinator nicht finden und beendet sich ganz traurig :(\n")
+		end,
+		GGTProzess ! {sendy, RandomY},
+		sendStartToClients(Arr, N-1, [ChoosenClient| UsedClients], ArrSize, Nameservice)
 	end.
 		
 
-loop() ->
+loop(GGTListe, Nameservice) ->
 	receive
 
 		{briefmi, {Clientname, CMi, CZeit}} -> %Ein ggT-Prozess mit Namen Clientname informiert ¸ber sein neues Mi CMi um CZeit Uhr.			
 			io:format("~p meldet neues Mi ~p um ~p. \n", [Clientname,CMi, CZeit]),
-			loop();
+			loop(GGTListe, Nameservice);
 		{briefterm, {Clientname, CMi, CZeit}} -> %Ein ggT-Prozess mit Namen Clientname informiert ¸ber ¸ber die Terminierung der Berechnung mit Ergebnis CMi um CZeit Uhr.
 			io:format("~p meldet TERMINIERUNG !!! mit Mi ~p um ~p. \n", [Clientname,CMi, CZeit]),
-			loop();
+			loop(GGTListe, Nameservice);
 		reset -> %Der Koordinator sendet allen ggT-Prozessen das kill-Kommando und bringt sich selbst in den initialen Zustand, indem sich Starter wieder melden kˆnnen.
 			ka;
 		kill -> %Der Koordinator wird beendet und sendet allen ggT-Prozessen das kill-Kommando.
-			ka
+			ka;
+		startecalc ->
+			io:format("Erzeuge Mis \n"),
+			createMis(GGTListe, Nameservice),
+			io:format("Mis erzeugt und beginne mit anstoﬂen \n"),
+			startCalc(GGTListe, Nameservice),
+			io:format("anstoﬂen finished \n"),
+			loop(GGTListe, Nameservice)
 	end.
